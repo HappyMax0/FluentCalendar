c537e8857bdb3c3a666d012e106cd59e8e5e96e7
HappyMax0 <Jihaoran12@hotmail.com>
2025-07-09 09:03:44 +0800

    Update WeekPage.xaml.cs

--- Views/WeekPage.xaml.cs
+++ Views/WeekPage.xaml.cs
@@ -1,20 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using CalendarWinUI3.Models.Utils;
-using CalendarWinUI3.Models;
-using Windows.Globalization;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System;
 using Windows.Storage;
 using DayOfWeek = System.DayOfWeek;
 
